95f494cf9962b7f5419e242c6d30d7d69980a1f6
v-zaycev <70699650+v-zaycev@users.noreply.github.com>
2025-04-07 01:40:44 +0300

    In progress

--- common/utilities.py
+++ common/utilities.py
@@ -1,7 +1,48 @@
 import pandas as pd
 import re
 import json
-from generate_text import summarize, async_summarize
+from common.generate_text import summarize, async_summarize
+
+class survey_structure:
+    def __init__(self, file_name : str) -> dict:
+        with open(file_name, "r", encoding = "utf-8") as tmp_input:
+            self.__survey_structure = json.load(tmp_input)    
+            new_fields = dict()
+            for key, value in self.__survey_structure["fields"].items():
+                new_fields[int(key)] = value
+            self.__survey_structure["fields"] = new_fields
+            new_output_headers = dict()
+            for key, value in self.__survey_structure["output headers"].items():
+                new_output_headers[int(key)] = value
+            self.__survey_structure["output headers"] = new_output_headers
+        
+    def __getitem__(self, key):
+        return self.__survey_structure[key]
+
+    def create_group_structure(self, columns : list) -> dict:
+        group_structure = dict()
+        for index in columns:
+            if self.__survey_structure["fields"][index][0] not in group_structure:
+                group_structure[self.__survey_structure["fields"][index][0]] = [index]
+            else:
+                group_structure[self.__survey_structure["fields"][index][0]].append(index)
+        return group_structure
+    
+    def create_person_template(self):
+        person_template = dict()
+        for index, info in self.__survey_structure["fields"].items():
+            if index in self.__survey_structure["output headers"]:
+                field_name = self.__survey_structure["output headers"][index]
+            else:
+                field_name = info[1]
+
+            if info[0] == "number":
+                person_template[index] = [field_name, number_collector(self.__survey_structure["empty_value"])] 
+            elif info[0] == "select":
+                person_template[index] = [field_name, select_collector(self.__survey_structure["empty_value"], info)] 
+            elif info[0] == "free":
+                person_template[index] = [field_name, free_collector(self.__survey_structure["empty_value"])]
+        return person_template
 
 class names_dict:
     def __init__(self, filename : str):
@@ -71,9 +112,6 @@ class names_dict:
             if len(elements) > 1 and not elements[1].startswith(first): continue
             if len(elements) > 2 and middle != None and not elements[2].startswith(middle): continue
             candidates.append(candidate_str)
-        
-
-
 
 class number_collector:
     def __init__(self, default_value):
